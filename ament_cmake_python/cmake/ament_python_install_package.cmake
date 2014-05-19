714afd7bb8215b65ea8716126c73bb2c05c645ee
Dirk Thomas <dthomas@osrfoundation.org>
2014-05-18 23:30:11 -0700

    add ament_cmake_python

---
+++ ament_cmake_python/cmake/ament_python_install_package.cmake
@@ -0,0 +1,49 @@
+#
+# Install a Python package (and its recursive subpackages).
+#
+# :param package_name: the Python package name
+# :type package_name: string
+# :param PACKAGE_DIR: the path to the Python package directory (default:
+#   <package_name> folder relative to the CMAKE_CURRENT_LIST_DIR)
+# :type PACKAGE_DIR: string
+#
+macro(ament_python_install_package)
+  _ament_cmake_python_register_environment_hook()
+  _ament_cmake_python_install_package(${ARGN})
+endmacro()
+
+function(_ament_cmake_python_install_package package_name)
+  cmake_parse_arguments(ARG "" "PACKAGE_DIR" "" ${ARGN})
+  if(ARG_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "ament_python_install_package() called with unused arguments: ${ARG_UNPARSED_ARGUMENTS}")
+  endif()
+
+  if(NOT ARG_PACKAGE_DIR)
+    set(ARG_PACKAGE_DIR "${CMAKE_CURRENT_LIST_DIR}/${package_name}")
+  endif()
+  if(NOT IS_ABSOLUTE ARG_PACKAGE_DIR)
+    set(ARG_PACKAGE_DIR "${CMAKE_CURRENT_LIST_DIR}/${ARG_PACKAGE_DIR}")
+  endif()
+
+  if(NOT EXISTS "${ARG_PACKAGE_DIR}/__init__.py")
+    message(FATAL_ERROR "ament_python_install_package() the Python package folder '${ARG_PACKAGE_DIR}' doesn't contain an '__init__.py' file")
+  endif()
+
+  _ament_cmake_python_register_environment_hook()
+
+  if(NOT PYTHON_INSTALL_DIR)
+    message(FATAL_ERROR "ament_python_install_package() variable 'PYTHON_INSTALL_DIR' must not be empty")
+  endif()
+  install(
+    DIRECTORY "${ARG_PACKAGE_DIR}/"
+    DESTINATION "${PYTHON_INSTALL_DIR}/${package_name}"
+  )
+  # TODO optionally compile Python files
+
+  list(FIND AMENT_CMAKE_PYTHON_INSTALL_INSTALLED_NAMES "${package_name}" index)
+  if(NOT index EQUAL -1)
+    message(FATAL_ERROR "ament_python_install_package() a Python module file or package with the same name '${package_name}' has been installed before")
+  endif()
+  list(APPEND AMENT_CMAKE_PYTHON_INSTALL_INSTALLED_NAMES "${package_name}")
+  set(AMENT_CMAKE_PYTHON_INSTALL_INSTALLED_NAMES "${AMENT_CMAKE_PYTHON_INSTALL_INSTALLED_NAMES}" PARENT_SCOPE)
+endfunction()
