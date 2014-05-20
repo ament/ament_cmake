1690281e161ae04cc9a93ad7943741835c5808f9
Dirk Thomas <dthomas@osrfoundation.org>
2014-05-20 09:29:08 -0700

    add ament_cmake_export_interfaces

---
+++ ament_cmake_export_interfaces/ament_cmake_export_interfaces-extras.cmake
@@ -0,0 +1,12 @@
+# copied from ament_cmake_export_interfaces/ament_cmake_export_interfaces-extras.cmake
+
+# register ament_package() hook for interfaces once
+macro(_ament_cmake_export_interfaces_register_package_hook)
+  if(NOT DEFINED _AMENT_CMAKE_EXPORT_INTERFACES_PACKAGE_HOOK_REGISTERED)
+    find_package(ament_cmake_core REQUIRED)
+    ament_register_extension("ament_package" "ament_cmake_export_interfaces" "ament_cmake_export_interfaces_package_hook.cmake")
+    set(_AMENT_CMAKE_EXPORT_INTERFACES_PACKAGE_HOOK_REGISTERED TRUE)
+  endif()
+endmacro()
+
+include("${ament_cmake_export_interfaces_DIR}/ament_export_interfaces.cmake")
---
+++ ament_cmake_export_interfaces/cmake/ament_cmake_export_interfaces_package_hook.cmake
@@ -0,0 +1,20 @@
+# generate and register extra file for interfaces
+set(_generated_extra_file "${CMAKE_CURRENT_BINARY_DIR}/ament_cmake_export_interfaces/ament_cmake_export_interfaces-extras.cmake")
+configure_file(
+  "${ament_cmake_export_interfaces_DIR}/ament_cmake_export_interfaces-extras.cmake.in"
+  "${_generated_extra_file}"
+  @ONLY
+)
+list(APPEND ${PROJECT_NAME}_CONFIG_EXTRAS "${_generated_extra_file}")
+
+# install export files for interfaces
+if(NOT "${_AMENT_CMAKE_EXPORT_INTERFACES}" STREQUAL "")
+  foreach(_interface ${_AMENT_CMAKE_EXPORT_INTERFACES})
+    install(
+      EXPORT "${_interface}"
+      DESTINATION share/${PROJECT_NAME}/cmake
+      NAMESPACE "${PROJECT_NAME}::"
+      FILE "${_interface}Export.cmake"
+    )
+  endforeach()
+endif()
