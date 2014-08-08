3819439aaee2d2f0b3f1deab001c16d694043ccb
Dirk Thomas <dthomas@osrfoundation.org>
2014-08-08 16:55:55 -0700

    add cmake function to read index

---
+++ ament_cmake_index/cmake/ament_index_get_resources.cmake
@@ -0,0 +1,25 @@
+#
+# Get all registered package resource of a specific type from the index.
+#
+# :param var: the output variable name
+# :type var: list of resource names
+# :param resource_type: the type of the resource
+# :type resource_type: string
+#
+# @public
+#
+function(ament_index_get_resources var resource_type)
+  if("${resource_type}" STREQUAL "")
+    message(FATAL_ERROR
+      "ament_index_get_resources() called without a 'resource_type'")
+  endif()
+
+  if(NOT "${ARGN}" STREQUAL "")
+    message(FATAL_ERROR "ament_index_get_resources() called with unused "
+      "arguments: ${ARGN}")
+  endif()
+
+  # TODO search in all workspaces
+  file(GLOB resources RELATIVE "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/${resource_type}" "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/${resource_type}/*")
+  set(${var} "${resources}" PARENT_SCOPE)
+endfunction()
