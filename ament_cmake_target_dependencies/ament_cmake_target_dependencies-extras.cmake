6a184a63f641910c8efd650e18972982cccc5a52
Dirk Thomas <dthomas@osrfoundation.org>
2014-08-29 15:49:13 -0700

    add ament_cmake_libraries

--- ament_cmake_target_dependencies/ament_cmake_target_dependencies-extras.cmake
+++ ament_cmake_target_dependencies/ament_cmake_target_dependencies-extras.cmake
@@ -1,5 +1,8 @@
 # copied from
 # ament_cmake_target_dependencies/ament_cmake_target_dependencies-extras.cmake
 
+find_package(ament_cmake_core REQUIRED)
+find_package(ament_cmake_libraries REQUIRED)
+
 include(
   "${ament_cmake_target_dependencies_DIR}/ament_target_dependencies.cmake")
--- ament_cmake_target_dependencies/cmake/ament_target_dependencies.cmake
+++ ament_cmake_target_dependencies/cmake/ament_target_dependencies.cmake
@@ -19,17 +19,27 @@ function(ament_target_dependencies target)
   endif()
   if(${ARGC} GREATER 0)
     message(" - ament_target_dependencies(${target} ${ARGN})")
+    set(definitions "")
+    set(include_dirs "")
+    set(libraries "")
     foreach(package_name ${ARGN})
       message(" - ament_target_dependencies(${target}) ${package_name}")
       if(NOT ${${package_name}_FOUND})
         message(FATAL_ERROR "ament_target_dependencies() the passed package name '${package_name}' was not found before")
       endif()
-      target_compile_definitions(${target}
-        PUBLIC ${${package_name}_DEFINITIONS})
-      target_include_directories(${target}
-        PUBLIC ${${package_name}_INCLUDE_DIRS})
-      target_link_libraries(${target}
-        PUBLIC ${${package_name}_LIBRARIES})
+      list_append_unique(definitions ${${package_name}_DEFINITIONS})
+      list_append_unique(include_dirs ${${package_name}_INCLUDE_DIRS})
+      list(APPEND libraries ${${package_name}_LIBRARIES})
     endforeach()
+    target_compile_definitions(${target}
+      PUBLIC ${definitions})
+    # TODO
+    #ament_order_include_directories(ordered_include_dirs ${include_dirs})
+    target_include_directories(${target}
+      PUBLIC ${include_dirs})
+      #PUBLIC ${ordered_include_dirs})
+    ament_libraries_deduplicate(unique_libraries ${libraries})
+    target_link_libraries(${target}
+      ${unique_libraries})
   endif()
 endfunction()
