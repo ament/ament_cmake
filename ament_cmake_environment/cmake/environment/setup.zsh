a4aae9768c8b51314a4fd4770477be82608cdd99
Dirk Thomas <dthomas@osrfoundation.org>
2014-09-18 22:41:30 -0700

    refactor to use templates provided by ament_package

--- ament_cmake_environment/cmake/environment/setup.zsh
+++
@@ -1,7 +0,0 @@
-# copied from ament_cmake_environment/cmake/environment/setup.zsh
-
-AMENT_SHELL=zsh
-
-# source setup.sh from same directory as this file
-AMENT_CURRENT_PREFIX=$(builtin cd -q "`dirname "${(%):-%N}"`" > /dev/null && pwd)
-. "$AMENT_CURRENT_PREFIX/setup.sh"
