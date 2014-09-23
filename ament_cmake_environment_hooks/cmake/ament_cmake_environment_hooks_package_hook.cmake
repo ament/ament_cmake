8ad0053fc3fbad2a927930a43c68a3fff7d9e72c
Dirk Thomas <dthomas@osrfoundation.org>
2014-09-23 10:57:05 -0700

    change the environment hooks to not be templates anymore

--- ament_cmake_environment_hooks/cmake/ament_cmake_environment_hooks_package_hook.cmake
+++ ament_cmake_environment_hooks/cmake/ament_cmake_environment_hooks_package_hook.cmake
@@ -1,6 +1,6 @@
 ament_environment_hooks(
-  "${ament_cmake_environment_hooks_DIR}/environment/ament_prefix_path.sh.in"
-  "${ament_cmake_environment_hooks_DIR}/environment/path.sh.in"
+  "${ament_cmake_environment_hooks_DIR}/environment/ament_prefix_path.sh"
+  "${ament_cmake_environment_hooks_DIR}/environment/path.sh"
 )
 
 if(AMENT_CMAKE_ENVIRONMENT_PACKAGE_GENERATION)
