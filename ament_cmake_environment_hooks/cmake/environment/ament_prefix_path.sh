90f1d397dd11a0912213fb72f7482e12a0fca73f
Dirk Thomas <dthomas@osrfoundation.org>
2014-09-23 10:57:05 -0700

    change the environment hooks to not be templates anymore

---
+++ ament_cmake_environment_hooks/cmake/environment/ament_prefix_path.sh
@@ -0,0 +1,4 @@
+# copied from
+# ament_cmake_environment_hooks/cmake/environment/ament_prefix_path.sh
+
+ament_prepend_unique_value AMENT_PREFIX_PATH "$AMENT_CURRENT_PREFIX"
---
+++ ament_cmake_environment_hooks/cmake/environment/path.sh
@@ -0,0 +1,3 @@
+# copied from ament_cmake_environment_hooks/cmake/environment/path.sh
+
+ament_prepend_unique_value PATH "$AMENT_CURRENT_PREFIX/bin"
