ab3e9ddc41aeea2658c59a001151392085572ad4
William Woodall <william@osrfoundation.org>
2015-03-10 23:00:24 -0700

    [windows] compact file extension logic

--- ament_cmake_core/cmake/environment_hooks/ament_cmake_environment_hooks_package_hook.cmake
+++ ament_cmake_core/cmake/environment_hooks/ament_cmake_environment_hooks_package_hook.cmake
@@ -1,14 +1,13 @@
 if(WIN32)
-    ament_environment_hooks(
-      "${ament_cmake_core_DIR}/environment_hooks/environment/ament_prefix_path.bat"
-      "${ament_cmake_core_DIR}/environment_hooks/environment/path.bat"
-    )
+    set(_ext .bat)
 else()
-    ament_environment_hooks(
-      "${ament_cmake_core_DIR}/environment_hooks/environment/ament_prefix_path.sh"
-      "${ament_cmake_core_DIR}/environment_hooks/environment/path.sh"
-    )
+    set(_ext .sh)
 endif()
+ament_environment_hooks(
+  "${ament_cmake_core_DIR}/environment_hooks/environment/ament_prefix_path${_ext}"
+  "${ament_cmake_core_DIR}/environment_hooks/environment/path${_ext}"
+)
+unset(_ext)
 
 if(AMENT_CMAKE_ENVIRONMENT_PACKAGE_GENERATION)
   ament_generate_package_environment()
