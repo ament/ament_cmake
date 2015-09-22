34290add670bcf6721cd326d9563c26b574e097d
Esteve Fernandez <esteve@apache.org>
2015-09-22 15:52:07 -0700

    Use VERSION_LESS to test the Nose version
    `VERSION_LESS` is used for checking versions:
    
    http://cmake.org/cmake/help/v2.8.12/cmake.html#command:if

--- ament_cmake_nose/cmake/ament_add_nose_test.cmake
+++ ament_cmake_nose/cmake/ament_add_nose_test.cmake
@@ -54,7 +54,7 @@ function(_ament_add_nose_test testname path)
   set(cmd
     "${NOSETESTS}" "${path}" "--with-xunit"
     "--xunit-file=${result_file}")
-  if(NOT "${NOSETESTS_VERSION}" LESS "1.3.5")
+  if(NOT "${NOSETESTS_VERSION}" VERSION_LESS "1.3.5")
     list(APPEND cmd "--xunit-testsuite-name=${PROJECT_NAME}.nosetests")
   endif()
 
