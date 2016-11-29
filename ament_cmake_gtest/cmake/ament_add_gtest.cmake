17ae94110beb040e0dff16d11753e623d62c6fea
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-11-29 09:02:01 -0800

    deduplicate ament_add_test call

--- ament_cmake_gtest/cmake/ament_add_gtest.cmake
+++ ament_cmake_gtest/cmake/ament_add_gtest.cmake
@@ -95,32 +95,26 @@ function(_ament_add_gtest target)
   if(ARG_WORKING_DIRECTORY)
     set(ARG_WORKING_DIRECTORY "WORKING_DIRECTORY" "${ARG_WORKING_DIRECTORY}")
   endif()
-
   if(ARG_SKIP_TEST)
-    ament_add_test(
-      "${target}"
-      COMMAND ${cmd}
-      OUTPUT_FILE "${CMAKE_BINARY_DIR}/ament_cmake_gtest/${target}.txt"
-      RESULT_FILE "${result_file}"
-      SKIP_TEST
-    )
-  else()
-    ament_add_test(
-      "${target}"
-      COMMAND ${cmd}
-      OUTPUT_FILE "${CMAKE_BINARY_DIR}/ament_cmake_gtest/${target}.txt"
-      RESULT_FILE "${result_file}"
-      ${ARG_ENV}
-      ${ARG_APPEND_ENV}
-      ${ARG_APPEND_LIBRARY_DIRS}
-      ${ARG_TIMEOUT}
-      ${ARG_WORKING_DIRECTORY}
-    )
-    set_tests_properties(
-      "${target}"
-      PROPERTIES
-      REQUIRED_FILES "${executable}"
-      LABELS "gtest"
-    )
+    set(ARG_SKIP_TEST "SKIP_TEST")
   endif()
+
+  ament_add_test(
+    "${target}"
+    COMMAND ${cmd}
+    OUTPUT_FILE "${CMAKE_BINARY_DIR}/ament_cmake_gtest/${target}.txt"
+    RESULT_FILE "${result_file}"
+    ${ARG_SKIP_TEST}
+    ${ARG_ENV}
+    ${ARG_APPEND_ENV}
+    ${ARG_APPEND_LIBRARY_DIRS}
+    ${ARG_TIMEOUT}
+    ${ARG_WORKING_DIRECTORY}
+  )
+  set_tests_properties(
+    "${target}"
+    PROPERTIES
+    REQUIRED_FILES "${executable}"
+    LABELS "gtest"
+  )
 endfunction()
