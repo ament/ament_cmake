fe0bbad710d40daa1563920874ab716f71851d33
dhood <dhood@users.noreply.github.com>
2017-04-20 09:54:01 -0700

    Add optional list of required packages for ament_auto_find_build_dependencies (#93)
    * Add optional list of required packages
    
    * Prefix ARG variables + fixup
    
    * REQUIRED_PACKAGES -> REQUIRED
    
    * Output all ignored packages at once
    
    * Pass REQUIRED in addition to QUIET, not instead of
    
    * _ignored_pacakges -> _additional_packages
    
    * De-duplicate the find_package call
    
    * rename var and small changes

--- ament_cmake_auto/cmake/ament_auto_find_build_dependencies.cmake
+++ ament_cmake_auto/cmake/ament_auto_find_build_dependencies.cmake
@@ -15,6 +15,11 @@
 #
 # Invoke find_package() for all build and buildtool dependencies.
 #
+# :param REQUIRED: an optional list of package names that are known
+# required CMake dependencies. For these dependencies, find_package() will be
+# invoked with REQUIRED.
+# :type REQUIRED: list of strings
+#
 # All found package names are appended to the
 # ``${PROJECT_NAME}_FOUND_BUILD_DEPENDS`` /
 # ``${PROJECT_NAME}_FOUND_BUILDTOOL_DEPENDS`` variables.
@@ -27,18 +32,39 @@
 # @public
 #
 macro(ament_auto_find_build_dependencies)
-  if(ARGN)
+  cmake_parse_arguments(_ARG "" "" "REQUIRED" ${ARGN})
+  if(_ARG_UNPARSED_ARGUMENTS)
     message(FATAL_ERROR "ament_auto_find_build_dependencies() called with "
-      "unused arguments: ${ARGN}")
+      "unused arguments: ${_ARG_UNPARSED_ARGUMENTS}")
   endif()
 
   if(NOT _AMENT_PACKAGE_NAME)
     ament_package_xml()
   endif()
 
+  # ensure that the caller isn't expecting packages not listed in the manifest
+  set(_unknown_packages "")
+  foreach(_package_name ${_ARG_REQUIRED})
+    if(NOT _package_name IN_LIST ${PROJECT_NAME}_BUILD_DEPENDS AND
+      NOT _package_name IN_LIST ${PROJECT_NAME}_BUILDTOOL_DEPENDS
+    )
+      list(APPEND _unknown_packages ${_package_name})
+    endif()
+  endforeach()
+  if(_unknown_packages)
+    string(REPLACE ";" ", " _unknown_packages_str "${_unknown_packages}")
+    message(FATAL_ERROR "ament_auto_find_build_dependencies() called with "
+      "required packages that are not listed as a build/buildtool dependency in "
+      "the package.xml: ${_unknown_packages_str}")
+  endif()
+
   # try to find_package() all build dependencies
   foreach(_dep ${${PROJECT_NAME}_BUILD_DEPENDS})
-    find_package(${_dep} QUIET)
+    set(_REQUIRED_KEYWORD "")
+    if(_dep IN_LIST _ARG_REQUIRED)
+      set(_REQUIRED_KEYWORD "REQUIRED")
+    endif()
+    find_package(${_dep} QUIET ${_REQUIRED_KEYWORD})
     if(${_dep}_FOUND)
       list(APPEND ${PROJECT_NAME}_FOUND_BUILD_DEPENDS ${_dep})
 
@@ -50,7 +76,11 @@ macro(ament_auto_find_build_dependencies)
 
   # try to find_package() all buildtool dependencies
   foreach(_dep ${${PROJECT_NAME}_BUILDTOOL_DEPENDS})
-    find_package(${_dep} QUIET)
+    set(_REQUIRED_KEYWORD "")
+    if(_dep IN_LIST _ARG_REQUIRED)
+      set(_REQUIRED_KEYWORD "REQUIRED")
+    endif()
+    find_package(${_dep} QUIET ${_REQUIRED_KEYWORD})
     if(${_dep}_FOUND)
       list(APPEND ${PROJECT_NAME}_FOUND_BUILDTOOL_DEPENDS ${_dep})
     endif()
