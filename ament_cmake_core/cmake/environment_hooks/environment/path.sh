88add8cab0ca579e89e95b3ab343af319c163195
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-08-09 05:41:21 -0700

    only add existing directories to PATH (#149)

--- ament_cmake_core/cmake/environment_hooks/environment/path.sh
+++ ament_cmake_core/cmake/environment_hooks/environment/path.sh
@@ -1,3 +1,5 @@
 # copied from ament_cmake_core/cmake/environment_hooks/environment/path.sh
 
-ament_prepend_unique_value PATH "$AMENT_CURRENT_PREFIX/bin"
+if [ -d "$AMENT_CURRENT_PREFIX/bin" ]; then
+  ament_prepend_unique_value PATH "$AMENT_CURRENT_PREFIX/bin"
+fi
