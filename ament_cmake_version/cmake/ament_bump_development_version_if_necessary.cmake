20f4243a97ebc9a157b994780da876dea059af08
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-10-16 17:52:08 -0700

    quote versions in message
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- ament_cmake_version/cmake/ament_bump_development_version_if_necessary.cmake
+++ ament_cmake_version/cmake/ament_bump_development_version_if_necessary.cmake
@@ -48,8 +48,8 @@ macro(ament_bump_development_version_if_necessary development_version)
 
   if("${${PROJECT_NAME}_VERSION}" VERSION_LESS "${development_version}")
     message(STATUS
-      "Overriding exported package version ${${PROJECT_NAME}_VERSION} with "
-      "development version ${development_version}")
+      "Overriding exported package version '${${PROJECT_NAME}_VERSION}' with "
+      "development version '${development_version}'")
     set(${PROJECT_NAME}_VERSION "${development_version}")
   endif()
 endmacro()
