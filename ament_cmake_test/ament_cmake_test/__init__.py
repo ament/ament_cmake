657f106fb3b470ca56f055307eba51ed863f55e8
Christophe Bedard <bedard.christophe@gmail.com>
2020-03-16 17:00:28 -0400

    Exclude gtests from pytest coverage explicitly
    They were excluded before, but only because gtests didn't use --env or --append-end.
    
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- ament_cmake_test/ament_cmake_test/__init__.py
+++ ament_cmake_test/ament_cmake_test/__init__.py
@@ -140,9 +140,10 @@ def _run_test(parser, args, failure_result_file, output_handle):
             output_handle.write((msg + '\n').encode())
             output_handle.flush()
 
+    env_os = dict(os.environ)
     env = None
     if args.env or args.append_env:
-        env = dict(os.environ)
+        env = env_os
         if args.env:
             log('-- run_test.py: extra environment variables:')
             previous_key = None
@@ -180,7 +181,13 @@ def _run_test(parser, args, failure_result_file, output_handle):
                 env[key] += value
                 previous_key = key
 
-        if args.pytest_with_coverage or 'AMENT_CMAKE_TEST_PYTEST_WITH_COVERAGE' in env:
+    # make sure this is not a gtest before considering enabling pytest coverage
+    if (
+        not any(command.startswith('--gtest_output=') for command in args.command) and
+        (args.pytest_with_coverage or 'AMENT_CMAKE_TEST_PYTEST_WITH_COVERAGE' in env_os)
+    ):
+        package_source_dir = (env or {}).get('AMENT_CMAKE_CURRENT_SOURCE_DIR')
+        if package_source_dir is not None:
             try:
                 from pytest_cov import __version__ as pytest_cov_version
             except ImportError:
@@ -189,37 +196,30 @@ def _run_test(parser, args, failure_result_file, output_handle):
                     'Test coverage will not be produced since '
                     "the pytest extension 'cov' was not found")
             else:
-                package_source_dir = env.get('AMENT_CMAKE_CURRENT_SOURCE_DIR', None)
-                if package_source_dir is None:
+                log('-- run_test.py: pytest coverage enabled')
+                # assuming that the build base path is at the end
+                build_base = env['PYTHONPATH'].split(':')[-1]
+                pytest_coverage_args = [
+                    '--cov=' + package_source_dir,
+                    '--cov-report=html:' + os.path.join(build_base, 'coverage.html'),
+                    '--cov-report=xml:' + os.path.join(build_base, 'coverage.xml'),
+                    '--cov-append',
+                ]
+                # use --cov-branch option only when available
+                # https://github.com/pytest-dev/pytest-cov/blob/v2.5.0/CHANGELOG.rst
+                if parse_version(pytest_cov_version) >= parse_version('2.5.0'):
+                    pytest_coverage_args += [
+                        '--cov-branch',
+                    ]
+                else:
                     log(
                         '-- run_test.py: '
-                        'Test coverage will not be produced since '
-                        'AMENT_CMAKE_CURRENT_SOURCE_DIR was not found')
-                else:
-                    log('-- run_test.py: pytest coverage enabled')
-                    # assuming that the build base path is at the end
-                    build_base = env['PYTHONPATH'].split(':')[-1]
-                    pytest_coverage_args = [
-                        '--cov=' + package_source_dir,
-                        '--cov-report=html:' + os.path.join(build_base, 'coverage.html'),
-                        '--cov-report=xml:' + os.path.join(build_base, 'coverage.xml'),
-                        '--cov-append',
-                    ]
-                    # use --cov-branch option only when available
-                    # https://github.com/pytest-dev/pytest-cov/blob/v2.5.0/CHANGELOG.rst
-                    if parse_version(pytest_cov_version) >= parse_version('2.5.0'):
-                        pytest_coverage_args += [
-                            '--cov-branch',
-                        ]
-                    else:
-                        log(
-                            '-- run_test.py: '
-                            'Test coverage will be produced but will not contain '
-                            'branch coverage information because the pytest '
-                            "extension 'cov' does not support it (need 2.5.0, "
-                            f'have {pytest_cov_version})')
-                    args.command += pytest_coverage_args
-                    env['COVERAGE_FILE'] = os.path.join(build_base, '.coverage')
+                        'Test coverage will be produced but will not contain '
+                        'branch coverage information because the pytest '
+                        "extension 'cov' does not support it (need 2.5.0, "
+                        f'have {pytest_cov_version})')
+                args.command += pytest_coverage_args
+                env['COVERAGE_FILE'] = os.path.join(build_base, '.coverage')
 
     log("-- run_test.py: invoking following command in '%s':\n - %s" %
         (os.getcwd(), ' '.join(args.command)))
