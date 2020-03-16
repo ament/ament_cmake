30769b3ddad47c92fa2a3574fe5b0bf3e989b4aa
Christophe Bedard <bedard.christophe@gmail.com>
2020-03-16 10:57:34 -0400

    Add --pytest-with-coverage to run_test.py and mention the env var
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- ament_cmake_test/ament_cmake_test/__init__.py
+++ ament_cmake_test/ament_cmake_test/__init__.py
@@ -74,6 +74,12 @@ def main(argv=sys.argv[1:]):
         action='store_true',
         default=False,
         help='Skip the test')
+    parser.add_argument(
+        '--pytest-with-coverage',
+        action='store_true',
+        default=False,
+        help='Generate coverage information for Python tests (alternatively,
+             'set AMENT_CMAKE_TEST_PYTEST_COVERAGE_ENABLED to any value)')
 
     if '--command' in argv:
         index = argv.index('--command')
@@ -174,7 +180,7 @@ def _run_test(parser, args, failure_result_file, output_handle):
                 env[key] += value
                 previous_key = key
 
-        if 'AMENT_CMAKE_TEST_PYTEST_COVERAGE_ENABLED' in env:
+        if args.pytest_with_coverage or 'AMENT_CMAKE_TEST_PYTEST_COVERAGE_ENABLED' in env:
             try:
                 from pytest_cov import __version__ as pytest_cov_version
             except ImportError:
