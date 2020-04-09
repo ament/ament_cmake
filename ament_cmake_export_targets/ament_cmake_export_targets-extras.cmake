8cdd38e361ba945fd3f10e86af8562caf48f54ad
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-04-08 20:54:45 -0700

    deprecate ament_export_interfaces() in favor of ament_export_targets() (#238)
    * duplicate ament_cmake_export_interfaces to ament_cmake_export_targets
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * update names in ament_cmake_export_targets after duplicating the files, add deprecation message for ament_export_interfaces(), add ament_cmake_export_targets to ament_cmake
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

---
+++ ament_cmake_export_targets/ament_cmake_export_targets-extras.cmake
@@ -0,0 +1,29 @@
+# Copyright 2014 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# copied from
+# ament_cmake_export_targets/ament_cmake_export_targets-extras.cmake
+
+# register ament_package() hook for targets once
+macro(_ament_cmake_export_targets_register_package_hook)
+  if(NOT DEFINED _AMENT_CMAKE_EXPORT_TARGETS_PACKAGE_HOOK_REGISTERED)
+    set(_AMENT_CMAKE_EXPORT_TARGETS_PACKAGE_HOOK_REGISTERED TRUE)
+
+    find_package(ament_cmake_core QUIET REQUIRED)
+    ament_register_extension("ament_package" "ament_cmake_export_targets"
+      "ament_cmake_export_targets_package_hook.cmake")
+  endif()
+endmacro()
+
+include("${ament_cmake_export_targets_DIR}/ament_export_targets.cmake")
