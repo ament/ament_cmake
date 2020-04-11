8c4ddc65ff7a1f124112053a436f4960a34daf9e
Steven! Ragnarök <nuclearsandwich@users.noreply.github.com>
2020-04-11 11:01:09 -0400

    [Dashing] Backport junit xml fixes for updated xunit plugin on c… (#230)
    * Add runner option to ament_add_test (#174)
    
    * ament_cmake allow speficiation of a different test runner
    
      - By default, still uses run_test.py
      - Example use case: ament_cmake_ros can use a test runner that
        sets a ROS_DOMAIN_ID
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>
    
    * ament_cmake move run_test.py to a python module
    
      - This should let us see the history
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>
    
    * ament_cmake refactor run_test.py into an importable python module
    
      - Adds an ament_cmake_test python package
    
    Signed-off-by: Pete Baughman <pete.baughman@apex.ai>
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>
    
    * improve handling of encoding (#181)
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>
    
    * use deterministic order for updated env vars (#196)
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>
    
    * Run tests in current binary directory, not global source directory (#206)
    
    Switch to CMAKE_CURRENT_BINARY_DIR for consistency with CTest
    
    Signed-off-by: Dan Rose <dan@digilabs.io>
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>
    
    * remove status attribute from result XML, add skipped tag instead (#218)
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>
    
    * Declare AMENT_TEST_RESULTS_DIR as a PATH (#221)
    
    Signed-off-by: Dan Rose <dan@digilabs.io>
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>
    
    * Generate xunit files valid for the junit10.xsd
    
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>
    
    * Revert "Run tests in current binary directory, not global source directory (#206)"
    
    This reverts commit 4354d62a68dc19f2b51cbbde1b0ee7cec3471ec9.
    
    Signed-off-by: Steven! Ragnarök <steven@nuclearsandwich.com>
    
    Co-authored-by: Peter Baughman <dblue135@yahoo.com>
    Co-authored-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    Co-authored-by: Dan Rose <rotu@users.noreply.github.com>
    Co-authored-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- ament_cmake_test/ament_cmake_test-extras.cmake
+++ ament_cmake_test/ament_cmake_test-extras.cmake
@@ -21,7 +21,7 @@ option(BUILD_TESTING "Build the testing tree." ON)
 # option()
 set(
   AMENT_TEST_RESULTS_DIR "${CMAKE_BINARY_DIR}/test_results"
-  CACHE STRING "The path where test results are generated"
+  CACHE PATH "The path where test results are generated"
 )
 
 if(BUILD_TESTING)
