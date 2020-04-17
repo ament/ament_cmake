c83cb39629e2e6dc86cd8702e2d0a304517b8bb6
Scott K Logan <logans@cottsay.net>
2020-04-16 19:01:14 -0700

    Drop duplicated <skipped/> element in result file
    The <skipped/> element was actually added as part of the
    `skipped_message` several lines earlier.
    
    While multiple <skipped/> elements doesn't violate the JUnit schema,
    there is no reason to have more than one.
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- ament_cmake_test/ament_cmake_test/__init__.py
+++ ament_cmake_test/ament_cmake_test/__init__.py
@@ -306,7 +306,7 @@ def _generate_result(result_file, *, failure_message=None, skip=False):
     return """<?xml version="1.0" encoding="UTF-8"?>
 <testsuite name="%s" tests="1" failures="%d" time="0" errors="0" skipped="%d">
   <testcase classname="%s" name="%s.missing_result" time="0">
-    %s%s%s
+    %s%s
   </testcase>
 </testsuite>\n""" % \
         (
@@ -314,7 +314,6 @@ def _generate_result(result_file, *, failure_message=None, skip=False):
             1 if failure_message else 0,
             1 if skip else 0,
             pkgname, testname,
-            '<skipped/>' if skip else '',
             failure_message, skipped_message
         )
 
