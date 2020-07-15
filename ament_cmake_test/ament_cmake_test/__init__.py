cba4e096d268b5d44b135300c0870821ce12632b
Scott K Logan <logans@cottsay.net>
2020-07-15 15:24:22 -0700

    Add SKIP_RETURN_CODE argument to ament_add_test (#264)
    This makes the `run_test.py` wrapper aware of the `SKIP_RETURN_CODE`
    property on CTest tests. In the existing implementation, the wrapper
    detects that no result file was generated and overrides the special
    return code coming from the test, making the the CTest feature fail
    completely.
    
    This change makes the wrapper script aware of the special return code,
    and when detected, will write a 'skipped' result file instead of a
    'failed' result file, and pass along the special return code as-is. Now
    the gtest result and the ctest results both show the test as 'skipped'
    when the special return flag is used.
    
    Note that none of this behavior is enabled by default, which is
    important because we wouldn't want a test to fail and return a code
    which we've decided is the special 'skip' return code. Only tests which
    are aware of this feature should use it.
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- ament_cmake_test/ament_cmake_test/__init__.py
+++ ament_cmake_test/ament_cmake_test/__init__.py
@@ -35,6 +35,14 @@ def separate_env_vars(env_str, env_argument_name, parser):
     return key, value
 
 
+def return_code(value):
+    value = int(value)
+    if value < 0 or value > 255:
+        raise argparse.ArgumentTypeError(
+            'Return code must be less than 256 and greater or equal to 0')
+    return value
+
+
 def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser(
         description='Run the test command passed as an argument and ensures'
@@ -72,6 +80,11 @@ def main(argv=sys.argv[1:]):
         action='store_true',
         default=False,
         help='Skip the test')
+    parser.add_argument(
+        '--skip-return-code',
+        type=return_code,
+        help="If the test returns this value and doesn't generate a result file, "
+             'create one stating that the test was skipped.')
 
     if '--command' in argv:
         index = argv.index('--command')
@@ -231,14 +244,19 @@ def _run_test(parser, args, failure_result_file, output_handle):
             content = h.read()
 
         if content == failure_result_file:
-            log("-- run_test.py: generate result file '%s' with failed test" % args.result_file,
-                file=sys.stderr)
-            # regenerate result file to include output / exception of the invoked command
-            failure_result_file = _generate_result(
-                args.result_file,
-                error_message='The test did not generate a result file:\n\n' + output)
+            if args.skip_return_code is not None and args.skip_return_code == rc:
+                log("-- run_test.py: generate result file '%s' with skipped test" % args.result_file)
+                # regenerate result file to indicate that the test was skipped
+                result_file = _generate_result(args.result_file, skip=True)
+            else:
+                log("-- run_test.py: generate result file '%s' with failed test" % args.result_file,
+                    file=sys.stderr)
+                # regenerate result file to include output / exception of the invoked command
+                result_file = _generate_result(
+                    args.result_file,
+                    error_message='The test did not generate a result file:\n\n' + output)
             with open(args.result_file, 'w') as h:
-                h.write(failure_result_file)
+                h.write(result_file)
         else:
             # prefix classname attributes
             if args.result_file.endswith('.gtest.xml') and args.package_name:
