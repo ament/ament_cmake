aed665d10ba0fc12aa7a0c9f0ce5239fda2f816c
Scott K Logan <logans@cottsay.net>
2020-09-10 10:43:51 -0700

    Update ament_cmake_google_benchmark-extras.cmake
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- ament_cmake_google_benchmark/ament_cmake_google_benchmark-extras.cmake
+++ ament_cmake_google_benchmark/ament_cmake_google_benchmark-extras.cmake
@@ -17,7 +17,7 @@ macro(_ament_cmake_google_benchmark_find_benchmark)
     set(_AMENT_CMAKE_GOOGLE_BENCHMARK_FIND_BENCHMARK TRUE)
 
     option(AMENT_RUN_PERFORMANCE_TESTS
-      "When not set to true, performance tests are unconditionally skipped" OFF)
+      "When disabled, performance tests are unconditionally skipped" OFF)
 
     find_package(benchmark QUIET)
 
