3ac0e1a2a377b032a5022fff38742111aca0a71c
Shane Loretz <sloretz@osrfoundation.org>
2021-12-28 11:54:42 -0800

    Don't quote
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- ament_cmake_include_directories/cmake/ament_include_directories_order.cmake
+++ ament_cmake_include_directories/cmake/ament_include_directories_order.cmake
@@ -27,7 +27,7 @@ macro(ament_include_directories_order var)
   if(WIN32)
     # Paths on windows may use back-slashes c:\Python3.8\...
     # Replace with forward-slashes so CMake doesn't treat them as escape characters below
-    string(REPLACE "\\" "/" _ament_all_arguments "${ARGN}")
+    string(REPLACE "\\" "/" _ament_all_arguments  ${ARGN})
   else()
     set(_ament_all_arguments "${ARGN}")
     string(REPLACE ":" ";" _ament_prefix_path_list "${_ament_prefix_path_list}")
