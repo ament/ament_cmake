e355e9ca44fd06d267052021e6b1ef59cb4b83cc
El Jawad Alaa <ejalaa12@gmail.com>
2023-03-23 13:16:14 +0100

    use the error handler replace to allow non-utf8 to be decoded (#381)
    * use the error handler replace to allow non-utf8 to be decoded
    
    Fixes #379
    
    Signed-off-by: Alaa <ejalaa12@gmail.com>
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>
    Co-authored-by: Michael Carroll <carroll.michael@gmail.com>

--- ament_cmake_test/ament_cmake_test/__init__.py
+++ ament_cmake_test/ament_cmake_test/__init__.py
@@ -210,7 +210,7 @@ def _run_test(parser, args, failure_result_file, output_handle):
                     decoded_line = line.decode(encoding)
                 except UnicodeDecodeError:
                     if i == len(encodings) - 1:
-                        raise
+                        decoded_line = line.decode(encoding, errors='replace')
                 else:
                     break
             print(decoded_line, end='')
