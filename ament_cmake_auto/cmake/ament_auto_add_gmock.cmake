bdb7ea6588200946795d9ffe740b07134a77bfe5
Jordan Palacios <jordan.palacios@pal-robotics.com>
2023-10-16 15:44:08 +0200

    Fixing copyright holder
    Signed-off-by: Jordan Palacios <jordan.palacios@pal-robotics.com>

--- ament_cmake_auto/cmake/ament_auto_add_gmock.cmake
+++ ament_cmake_auto/cmake/ament_auto_add_gmock.cmake
@@ -1,4 +1,4 @@
-# Copyright 2021 Whitley Software Services, LLC
+# Copyright 2023 PAL Robotics S.L.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
