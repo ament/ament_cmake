9c1c3c8475402be60cec6c33f67b328182742e05
Shane Loretz <sloretz@intrinsic.ai>
2025-03-19 01:15:18 +0000

    misc fixes
    Signed-off-by: Shane Loretz <sloretz@intrinsic.ai>

--- ament_cmake_auto/cmake/ament_auto_depend_on_packages.cmake
+++ ament_cmake_auto/cmake/ament_auto_depend_on_packages.cmake
@@ -27,7 +27,9 @@
 #   modern CMake targets.
 # :type SYSTEM: None
 # :param SCOPE: Optional. If given it must be one of PUBLIC, PRIVATE, or INTERFACE.
-#   See target_link_libraries() documentation for more info about SCOPE.
+#   See target_link_libraries() documentation for more info about SCOPE. If unset
+#   then it defaults to unset for CMake functions that support that, and PUBLIC for
+#   functions that don't.
 # :type SCOPE: string
 # :param PACKAGES: a list of package names
 # :type PACKAGES: list of strings
@@ -53,25 +55,41 @@ function(ament_auto_depend_on_packages target)
     message(FATAL_ERROR "If SCOPE is specified, it must be one of: ${allowed_scopes}. Got: '${ARG_SCOPE}'")
   endif()
 
+  if(ARG_SYSTEM)
+    # convert bool -> string that is passed to target_include_directories
+    set(ARG_SYSTEM "SYSTEM")
+  endif()
+
+  # Most target_*() macros require a keyword, but
+  # target_link_libraries has special behavior when the keyword is
+  # unset, so default to PUBLIC for most functions when unset
+  set(_implied_scope ${ARG_SCOPE})
+  if(NOT _implied_scope)
+    set(_implied_scope "PUBLIC")
+  endif()
+
   foreach(package_name ${ARG_PACKAGES})
     if(NOT "${${package_name}_FOUND}")
       message(FATAL_ERROR "'${package_name}' must be found with find_package() prior to passing it to ament_auto_depend_on_packages()")
     endif()
 
-    if("${${package_name}_TARGETS}")
+    if(${package_name}_TARGETS)
       # Use modern CMake targets
       target_link_libraries(${target} ${ARG_SCOPE} ${${package_name}_TARGETS})
     else()
       # Use standard CMake variables
       # https://cmake.org/cmake/help/latest/manual/cmake-developer.7.html#standard-variable-names
-      if("${${package_name}_INCLUDE_DIRS}")
-        target_include_directories(${target} ${ARG_SYSTEM} ${ARG_SCOPE} ${${package_name}_INCLUDE_DIRS})
+      if(${package_name}_INCLUDE_DIRS)
+        target_include_directories(${target} ${ARG_SYSTEM} ${_implied_scope} ${${package_name}_INCLUDE_DIRS})
       endif()
-      if("${${package_name}_LIBRARIES}")
+      if(${package_name}_LIBRARIES)
         target_link_libraries(${target} ${ARG_SCOPE} ${${package_name}_LIBRARIES})
       endif()
-      if("${${package_name}_DEFINITIONS}")
-        target_compile_definitions(${target} ${ARG_SCOPE} ${${package_name}_DEFINITIONS})
+      if(${package_name}_LIBRARY_DIRS)
+        target_link_directories(${target} ${_implied_scope} ${${package_name}_LIBRARY_DIRS})
+      endif()
+      if(${package_name}_DEFINITIONS)
+        target_compile_definitions(${target} ${_implied_scope} ${${package_name}_DEFINITIONS})
       endif()
     endif()
   endforeach()
