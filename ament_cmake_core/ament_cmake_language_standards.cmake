6e77d446bcd3622395cc63bbc3f4728cc9c7e846
Michael Carlstrom <rmc@carlstrom.com>
2026-03-22 09:29:26 -0700

    Add ament_set_default_language_standards
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

---
+++ ament_cmake_core/ament_cmake_language_standards.cmake
@@ -0,0 +1,29 @@
+# Copyright 2026 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+macro(ament_set_default_language_standards)
+  # C++
+  if(NOT DEFINED CMAKE_CXX_STANDARD)
+    set(CMAKE_CXX_STANDARD 17)
+  endif()
+
+  if(NOT DEFINED CMAKE_CXX_STANDARD_REQUIRED)
+    set(CMAKE_CXX_STANDARD_REQUIRED ON)
+  endif()
+
+  # C
+  if(NOT DEFINED CMAKE_C_STANDARD)
+    set(CMAKE_C_STANDARD 17)
+  endif()
+endmacro()
